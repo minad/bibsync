e4200e295b065a2c511a7b060ebc6b50a737bd47
Daniel Mendler <mail@daniel-mendler.de>
2013-04-07 11:48:02 +0200

    add jabref test

--- lib/bibsync/actions/jabref_format.rb
+++ lib/bibsync/actions/jabref_format.rb
@@ -13,7 +13,7 @@ module BibSync
         if File.read(@bib.file, 80) !~ /JabRef/
           notice 'Transforming file with JabRef'
           tmp_file = "#{@bib.file}.tmp.bib"
-          `jabref --nogui --import #{Shellwords.escape @bib.file} --output #{Shellwords.escape tmp_file} 2>&1 >/dev/null`
+          `jabref --primp #{Shellwords.escape File.join(File.dirname(__FILE__), 'jabref.xml')} --nogui --import #{Shellwords.escape @bib.file} --output #{Shellwords.escape tmp_file} 2>&1 >/dev/null`
           File.rename(tmp_file, @bib.file) if File.exists?(tmp_file)
         end
       end
--- test/actions/test_jabref_format.rb
+++ test/actions/test_jabref_format.rb
@@ -1,4 +1,26 @@
 require 'helper'
 
 describe BibSync::Actions::JabRefFormat do
+  before do
+    @tmpfile = File.join(fixturedir, 'tmp.bib')
+    FileUtils.copy(File.join(fixturedir, 'test.bib'), @tmpfile)
+  end
+
+  after do
+    File.unlink(@tmpfile) if File.exists?(@tmpfile)
+  end
+
+  let(:bib) do
+    BibSync::Bibliography.new(@tmpfile)
+  end
+
+  let(:action) do
+    BibSync::Actions::JabRefFormat.new(bib: bib)
+  end
+
+  it 'should format file' do
+    action.run
+    puts File.read(@tmpfile)
+    File.read(@tmpfile).must_match(/created\s+with\s+JabRef/)
+  end
 end
