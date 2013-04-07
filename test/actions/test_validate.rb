2e331e5b766096dc865ef76de729de16491480a0
Daniel Mendler <mail@daniel-mendler.de>
2013-04-07 12:11:43 +0200

    add simple test

--- test/actions/test_validate.rb
+++ test/actions/test_validate.rb
@@ -1,4 +1,15 @@
 require 'helper'
 
 describe BibSync::Actions::Validate do
+  let(:bib) do
+    BibSync::Bibliography.new(File.join(fixturedir, 'test.bib'))
+  end
+
+  let(:action) do
+    BibSync::Actions::Validate.new(bib: bib)
+  end
+
+  it 'should not raise an exception' do
+    action.run
+  end
 end
